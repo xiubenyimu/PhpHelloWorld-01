9e3ef1b3f4898c6de230f314a32b2c34013db6d2
xiubenyimu <paper4deliver@gmail.com>
2020-07-02 14:36:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <html>
- <head>
-  <title>Testing Page</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- </body>
+<body>
+
+<form action="upload_file.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" /> 
+<br />
+<input type="submit" name="submit" value="Submit" />
+</form>
+
+</body>
 </html>
