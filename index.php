d1bd9b01c4a12201fb42f2fc345da65351c6523b
xiubenyimu <paper4deliver@gmail.com>
2020-07-02 17:19:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 
 <form action="upload_file.php" method="post"
 enctype="multipart/form-data">
+<select name="stuid">
+    <option value="0">select number:</option>
+    <option value="1">001</option>
+    <option value="2">002</option>
+    <option value="3">003</option>
+    </select>
 <label for="file">Filename:</label>
 <input type="file" name="file" id="file" /> 
 <input type="date" name="date" />
